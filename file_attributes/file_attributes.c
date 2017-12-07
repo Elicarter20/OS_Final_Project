a18e5297a4a0deb7a2ff3dcdac9b286a6cf62b12
ranjankhadka123 <ranjan.khadka@bison.howard.edu>
2017-12-07 12:18:27 -0800

    First two syscall functions

--- file_attributes/file_attributes.c
+++ file_attributes/file_attributes.c
@@ -1,5 +1,18 @@
+#include <string.h>
+#include <unistd.h>
+#include <dirent.h>
 #include <linux/kernel.h>
-
+#include <linux/syscall.h>
+#include <linux/iofile.h>
+#include <linux/errno.h>
+#include <linux/linkage.h>
+#include <linux/fcntl.h>
+#include <linux/spinlock.h>
+#include <linux/uaccess.h>
+#include <linux/slab.h>
+#include <libgen.h>
+#include <sys/types.h>
+#include <sys/stat.h>
  asmlinkage long sys_hello(void)
 {
         printk(“Hello world\n”);
@@ -10,18 +23,21 @@
 
 asmlinkage long cs401_set_attribute(char *filename, char  *attrname, char *attrvalue, int size)
 {
-	
-	char* new_dir = "." + filename + "_attr" + NULL;//creates name of new directory
-	
-
-	if (true)
-	{
-		return 0;
+	unsigned int string_length = stren_user(basename(filename)) + 7 //7 bits for . , _attr and a NUll pointer.
+	char* new_dir = (char*)malloc(string_length*sizeof(char));
+	new_dir = "." + filename + "_attr" + NULL;//creates name of new directory
+	char* dir_path = dirname(filename);
+	/*
+	Check if the attribute directory exists. IF no attribute directory exists, then create a
+	new one */
+	if(opendir(dir_path) == NULL){
+		mkdir(dir_path, S_IRUSR | S_IWUSR | S_IXUR);
 	}
-	else
-	{
-		return -1;
-	}
+	char* attribute_path = dir_path + attrname;
+	//open file description and write attribute data into the new file.
+	int fp = open(attribute_path, O_RDWR | O_CREAT, S_IWUSR | S_IRUSR);
+	write(fp, attrvalue, size);
+	return 0;
 	
 }
 /*This call sets the attribute named “attrname” to the value in “attrvalue” which has length “size” to the file named “filename”. 
@@ -30,7 +46,30 @@ The filename must be an absolute path to the file. The strings should be null te
 */
 asmlinkage long cs401_get_attribute(char *filename, char *attrname, char *buf, int bufsize)
 {
-
+	unsigned int string_length = stren_user(basename(filename)) + 7 //7 bits for . , _attr and a NUll pointer.
+	char* new_dir = (char*)malloc(string_length*sizeof(char));
+	new_dir = "." + filename + "_attr" + NULL;//creates name of new directory
+	char* dir_path = dirname(filename);
+	if(opendir(path) == NULL){
+		return -1;
+	}
+	struct stat *std;
+	char* attribute_path = new_dir + attrname;
+	/*Obtain the information about the file and write it to the area pointed by std pointer.*/
+	if(int stat(new_dir, std) != -1){
+		unsigned int size = std.st_size;
+		if(bufsize == NULL){
+			fprintf(stderr, "out of memory\n");
+			exit(1);
+		} else if (bufsize < size){
+			return -1;
+		}
+		int fp = open(attribute_path, O_RDWR);
+		int con = read(fp, buf, bufsize);
+		return con;
+	} else {
+		return -1;
+	}
 }
 /*This call gets the value of the attribute named “attrname” attached to the file named “filename”. The value is stored in the buf pointer which must have at least “bufsize” by
 tes of storage. 
